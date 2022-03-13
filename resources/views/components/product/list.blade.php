96d5e80f9bfbb73bf9edb067ca8b926b955efad2
kevinsudut <kevinsuryaw@gmail.com>
2022-03-13 11:25:45 +0700

    Empty page product

--- resources/views/components/product/list.blade.php
+++ resources/views/components/product/list.blade.php
@@ -1,3 +1,29 @@
+<div class="card card-body mb-3">
+    <form action="" method="get">
+        <div class="row g-3 align-items-center">
+            <div class="col-md-3">
+                <label for="" class="form-label fw-bold">Start Time</label>
+                <input type="datetime-local" class="form-control" name="start_time" id="end-time" value="{{ request()->get('start_time') ?? '' }}">
+            </div>
+            <div class="col-md-3">
+                <label for="" class="form-label fw-bold">End Time</label>
+                <input type="datetime-local" class="form-control" name="end_time" id="end-time" value="{{ request()->get('end_time') ?? '' }}">
+            </div>
+            <div class="col-md-3">
+                <label for="" class="form-label fw-bold">Minimum Bid</label>
+                <input type="number" class="form-control" name="minimum_bid" id="minimum-bid" min="0" value="{{ request()->get('minimum_bid') ?? 0 }}">
+            </div>
+            <div class="col-md-3">
+                <label for="" class="form-label fw-bold">Maximum Bid</label>
+                <input type="number" class="form-control" name="maximum_bid" id="maximum-bid" min="0" value="{{ request()->get('maximum_bid') ?? 0 }}">
+            </div>
+        </div>
+        <div class="my-2">
+            <button class="btn btn-primary form-control">Filter Product</button>
+        </div>
+    </form>
+</div>
+
 <div class="row">
     @foreach ($products as $product)
         <div class="col-lg-3 col-md-6 col-sm-6">
--- resources/views/product/my-product/index.blade.php
+++ resources/views/product/my-product/index.blade.php
@@ -4,32 +4,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="card card-body mb-3">
-        <form action="" method="get">
-            <div class="row g-3 align-items-center">
-                <div class="col-md-3">
-                    <label for="" class="form-label fw-bold">Start Time</label>
-                    <input type="datetime-local" class="form-control" name="start_time" id="end-time" value="{{ request()->get('start_time') ?? '' }}">
-                </div>
-                <div class="col-md-3">
-                    <label for="" class="form-label fw-bold">End Time</label>
-                    <input type="datetime-local" class="form-control" name="end_time" id="end-time" value="{{ request()->get('end_time') ?? '' }}">
-                </div>
-                <div class="col-md-3">
-                    <label for="" class="form-label fw-bold">Minimum Bid</label>
-                    <input type="number" class="form-control" name="minimum_bid" id="minimum-bid" min="0" value="{{ request()->get('minimum_bid') ?? 0 }}">
-                </div>
-                <div class="col-md-3">
-                    <label for="" class="form-label fw-bold">Maximum Bid</label>
-                    <input type="number" class="form-control" name="maximum_bid" id="maximum-bid" min="0" value="{{ request()->get('maximum_bid') ?? 0 }}">
-                </div>
-            </div>
-            <div class="my-2">
-                <button class="btn btn-primary form-control">Filter Product</button>
-            </div>
-        </form>
-    </div>
-
-    @include('components.product.list', ['products' => $products])
+    @if (count($products) == 0)
+        @component('components.message.message')
+            @slot('type', 'primary')
+            @slot('heading', 'There is no product yet')
+        @endcomponent
+    @else
+        @include('components.product.list', ['products' => $products])
+    @endif
 
 @endsection
